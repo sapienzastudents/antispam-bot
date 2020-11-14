72d4ce1d78d8199a1e830e194f261dcdfdce8e8c
Enrico204 <enrico204@gmail.com>
2020-11-15 00:29:54 +0100

    Do not SIGKILL admins

--- on-sigkill.go
+++ on-sigkill.go
@@ -6,28 +6,33 @@ import (
 )
 
 func onSigKill(m *tb.Message, settings ChatSettings) {
-	if m.ReplyTo == nil || !settings.BotEnabled || m.Private() {
+	if m.ReplyTo == nil || !settings.BotEnabled || m.Private() || !botdb.IsGlobalAdmin(m.Sender) {
+		return
+	}
+
+	// If the user is an admin, be polite (remember: The Admin Is Always Right®)
+	isAdmin, err := IsAdminOf(m.Chat, m.ReplyTo.Sender)
+	if err != nil || isAdmin {
+		if err != nil {
+			logger.Critical(err)
+		}
 		return
 	}
 
 	logger.Debugf("Sigkill %d (%s %s %s) for %d (%s %s %s)", m.Sender.ID, m.Sender.Username, m.Sender.FirstName, m.Sender.LastName,
 		m.ReplyTo.Sender.ID, m.ReplyTo.Sender.Username, m.ReplyTo.Sender.FirstName, m.ReplyTo.Sender.LastName)
 
-	if botdb.IsGlobalAdmin(m.Sender) {
-		_, _ = b.Reply(m.ReplyTo, "You will be terminated in 60 seconds, there will be no further warnings")
+	_, _ = b.Reply(m.ReplyTo, "You will be terminated in 60 seconds, there will be no further warnings")
 
-		go func() {
-			time.Sleep(60 * time.Second)
+	go func() {
+		time.Sleep(60 * time.Second)
 
-			member, err := b.ChatMemberOf(m.Chat, m.ReplyTo.Sender)
-			if err != nil {
-				logger.Errorf("Can't ban user %d: %s", m.ReplyTo.Sender, err)
-				return
-			}
+		member, err := b.ChatMemberOf(m.Chat, m.ReplyTo.Sender)
+		if err != nil {
+			logger.Errorf("Can't ban user %d: %s", m.ReplyTo.Sender, err)
+			return
+		}
 
-			_ = b.Ban(m.Chat, member)
-		}()
-	} else {
-		logger.Warningf("User %d is not a global admin, skill request denied", m.Sender.ID)
-	}
+		_ = b.Ban(m.Chat, member)
+	}()
 }
