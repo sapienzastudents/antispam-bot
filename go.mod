8b116cc6ce4551996cc54a2adb691dd98f7fef62
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2021-11-02 11:55:29 +0100

    Use ardanlabs/conf/v2 to handle options

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.16
 
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/ardanlabs/conf/v2 v2.1.1
 	github.com/gliderlabs/ssh v0.3.1 // indirect
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/google/uuid v1.3.0
-	github.com/joho/godotenv v1.4.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.11.0
--- main.go
+++ main.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/go-redis/redis/v8"
-	"github.com/pkg/errors"
 	"gitlab.com/sapienzastudents/antispam-telegram-bot/service/botdatabase"
 	"gitlab.com/sapienzastudents/antispam-telegram-bot/service/cas"
 	"gitlab.com/sapienzastudents/antispam-telegram-bot/service/tbot"
 
-	"github.com/joho/godotenv"
+	"github.com/ardanlabs/conf/v2"
+	"github.com/go-redis/redis/v8"
 	"github.com/sirupsen/logrus"
 )
-
-const DefaultRedisURL = "redis://127.0.0.1:6379"
 
 // AppVersion is the app version injected by the compiler
 var AppVersion = "dev"
@@ -35,85 +31,115 @@ func main() {
 	}
 }
 
+// Bot configuration.
+type Config struct {
+	BotToken      string `conf:"default:-,flag:bot-token,short:b,help:Bot token"`
+	RedisURL      string `conf:"default:redis://127.0.0.1:6379,flag:redis-url,short:r,help:redis URL"`
+	GitTmpDir     string `conf:"default:-,flag:git-dir,help:git temporary director"`
+	GitSSHKey     string `conf:"default:-,flag:git-ssh-key,help:SSH key used with git"`
+	GitSSHKeyPass string `conf:"default:-,flag:git-ssh-key-pass,help:SSH key's password"`
+	CASUpdate     bool   `conf:"default:true,flag:cas-update,help:Update automatically CAS database"`
+}
+
+// Returns the loaded configuration, read from both command line and environment
+// variables.
+func getConfig() (Config, error) {
+	cfg := Config{}
+	help, err := conf.Parse("", &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			os.Exit(0)
+		}
+		return cfg, fmt.Errorf("parsing config: %w", err)
+	}
+
+	// Clean these variables for security purposes.
+	if err := os.Setenv("GIT_SSH_KEY_PASS", ""); err != nil {
+		return cfg, err
+	}
+	if err := os.Setenv("GIT_SSH_KEY", ""); err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+// Initializes and starts the bot.
 func run() error {
-	// Initialize environment
-	rand.Seed(time.Now().UnixNano())
-	_ = godotenv.Load()
-
-	// Create buffered channel for signals
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGTERM)
-
-	// Logging init
-	logrusLogger := logrus.New()
-	logrusLogger.SetOutput(os.Stdout)
-	logrusLogger.SetLevel(logrus.DebugLevel) // TODO
-	// TODO: logrusLogger.SetFormatter(&logrus.JSONFormatter{})
-
-	// Setting the hostname if available
-	hostname, _ := os.Hostname()
-	logger := logrusLogger.WithFields(logrus.Fields{
-		"hostname": hostname,
-	})
-
-	logger.Info("Antispam Telegram Bot, version ", AppVersion, " build ", BuildDate)
-
-	// Initalizing redis connection
-	logger.Info("Initializing redis DB connection")
-
-	rawURL, ok := os.LookupEnv("REDIS_URL")
-	if !ok {
-		rawURL = DefaultRedisURL
-	}
-	redisOptions, err := redis.ParseURL(rawURL)
+	// Initialize configuration.
+	cfg, err := getConfig()
 	if err != nil {
-		return errors.Wrap(err, "unable to parse REDIS_URL variable")
-	}
-	redisDb := redis.NewClient(redisOptions)
-	err = redisDb.Ping(context.TODO()).Err()
-	if err != nil {
-		return errors.Wrap(err, "unable to connect to Redis server")
-	}
-	botdb, err := botdatabase.New(redisDb)
-	if err != nil {
-		return errors.Wrap(err, "error creating DB connection")
+		return err
 	}
 
-	// Initializing CAS database
-	casDB, err := cas.New(os.Getenv("DISABLE_CAS") == "", logger, nil)
+	// Inizialite logger.
+	log := logrus.New()
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logrus.DebugLevel)
+
+	log.Info("Antispam Telegram Bot, version ", AppVersion, " build ", BuildDate)
+
+	// Initialize redis connection.
+	log.Info("Initializing redis DB connection")
+
+	redisOptions, err := redis.ParseURL(cfg.RedisURL)
 	if err != nil {
-		return errors.Wrap(err, "error creating CAS")
+		return fmt.Errorf("failed to parse redis URL: %w", err)
+	}
+	redisDB := redis.NewClient(redisOptions)
+	if err := redisDB.Ping(context.TODO()).Err(); err != nil {
+		return fmt.Errorf("failed to connect to redis server")
 	}
 
-	// Initialize Telegram
-	logger.Info("Initializing Telegram bot connection")
+	log.Info("Initializing database")
+	botdb, err := botdatabase.New(redisDB)
+	if err != nil {
+		return fmt.Errorf("failed to create DB connection: %w", err)
+	}
+
+	// Initialize CAS database.
+	log.Info("Initializing CAS database")
+	casDB, err := cas.New(cfg.CASUpdate, log, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create CAS database: %w", err)
+	}
+
+	// Initialize Telegram bot.
+	log.Info("Initializing Telegram bot connection")
 	bot, err := tbot.New(tbot.Options{
-		Logger:              logrusLogger,
+		Logger:              log,
 		Database:            botdb,
-		Token:               os.Getenv("BOT_TOKEN"),
+		Token:               cfg.BotToken,
 		CAS:                 casDB,
-		GitTemporaryDir:     os.Getenv("GIT_TEMP_DIR"),
-		GitSSHKeyFile:       os.Getenv("GIT_SSH_KEY"),
-		GitSSHKeyPassphrase: os.Getenv("GIT_SSH_KEY_PASS"),
+		GitTemporaryDir:     cfg.GitTmpDir,
+		GitSSHKeyFile:       cfg.GitSSHKey,
+		GitSSHKeyPassphrase: cfg.GitSSHKeyPass,
 	})
 	if err != nil {
-		return errors.Wrap(err, "error creating bot")
+		return fmt.Errorf("failed to create bot: %w", err)
 	}
-
-	_ = os.Setenv("GIT_SSH_KEY_PASS", "")
-	_ = os.Setenv("GIT_SSH_KEY", "")
 
 	go func() {
-		// Temporary HTTP Server for metrics
+		// Temporary HTTP Server for metrics.
+		log.Infof("Starting HTTP server for metrics on 0.0.0.0:3000")
 		http.Handle("/metrics", bot.MetricsHandler())
 		_ = http.ListenAndServe(":3000", nil)
 	}()
 
+	// Create buffered channel to catch SIGTERM signal.
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGTERM)
+
 	go func() {
 		<-sigchan
-		_ = bot.Close()
+		if err := bot.Close(); err != nil {
+			log.WithError(err).Error("failed to gracefully stop bot")
+		}
 	}()
 
-	err = bot.ListenAndServe()
-	return errors.Wrap(err, "error starting bot")
+	if err := bot.ListenAndServe(); err != nil {
+		return fmt.Errorf("failed to start bot")
+	}
+	return nil
 }
