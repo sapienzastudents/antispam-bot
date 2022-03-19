d0e39a83aa87e51439e35a92fa5cebe691b0c874
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2022-03-19 14:49:58 +0100

    Leave group if added by a non group or bot admin

--- service/bot/on-user-joined.go
+++ service/bot/on-user-joined.go
@@ -1,6 +1,9 @@
 package bot
 
-import tb "gopkg.in/telebot.v3"
+import (
+	"github.com/sirupsen/logrus"
+	tb "gopkg.in/telebot.v3"
+)
 
 // onAddedToGroup is fired when the bot is added to a group.
 func (bot *telegramBot) onAddedToGroup(ctx tb.Context, settings chatSettings) {
@@ -9,9 +12,50 @@ func (bot *telegramBot) onAddedToGroup(ctx tb.Context, settings chatSettings) {
 		bot.logger.WithField("updateid", ctx.Update().ID).Warn("Update with nil on Chat, ignored")
 		return
 	}
+	msg := ctx.Message()
+	if msg == nil {
+		bot.logger.WithField("updateid", ctx.Update().ID).Warn("Update with nil on Message, ignored")
+		return
+	}
+	sender := ctx.Sender()
+	if msg == nil {
+		bot.logger.WithField("updateid", ctx.Update().ID).Warn("Update with nil on Message, ignored")
+		return
+	}
+
+	logger := bot.logger.WithFields(logrus.Fields{
+		"chat_id":    chat.ID,
+		"chat_title": chat.Title,
+		"user_id":    sender.ID,
+	})
+
+	// Only bot admins or groups admins can add the bot to a group.
+	cmember, err := bot.telebot.ChatMemberOf(chat, sender)
+	if err != nil {
+		logger.WithError(err).Error("Failed to check user's member status on this chat")
+		return
+	}
+
+	isAdmin, err := bot.db.IsBotAdmin(sender.ID)
+	if err != nil {
+		logger.WithError(err).Error("Failed to check if user is a bot admin")
+		return
+	}
+
+	if !isAdmin && (cmember.Role != tb.Creator && cmember.Role != tb.Administrator) {
+		// This will trigger an update with the bot on UserLeft field, it
+		// will be used to remove chat's info from the DB.
+		if err := bot.telebot.Leave(chat); err != nil {
+			logger.WithError(err).Error("Failed to leave from this chat")
+			return
+		}
+		logger.Warn("A non-admin invited the bot on a group: leaving from this chat")
+		return
+	}
+
 	// Do nothing: the previous chained handler (refreshDBInfo) will take care
 	// of creating the new chat in the DB.
-	bot.logger.WithField("chatid", chat.ID).Info("Joining chat")
+	logger.Info("Joining chat")
 }
 
 // onUserJoined is fired when a user is added (or joins) to a group.
